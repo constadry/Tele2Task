73bc79674643854107feb8c9a69567d65922a0df
constadry <behappydtworry@gmail.com>
2022-03-05 17:01:18 +0300

    feat: interservice interaction

---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+using Tele2Task.Contexts;
+
+namespace Tele2Task.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
