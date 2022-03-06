aafdc581a77a89018dd37b661efce46f1f44ff48
constadry <behappydtworry@gmail.com>
2022-03-06 11:37:06 +0300

    feat: init the db when app's starting

--- SampleData.cs
+++ SampleData.cs
@@ -1,6 +1,20 @@
+using System.Diagnostics;
+using System.Text.Json;
+using Tele2Task.Contexts;
+using Tele2Task.Models;
+
 namespace Tele2Task;
 
 public class SampleData
 {
-    
+    public static async Task Initialize(AppDbContext context)
+    {
+        var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync("http://testlodtask20172.azurewebsites.net/task");
+        var userContent = await response.Content.ReadAsStringAsync();
+        var users = (JsonSerializer.Deserialize<IEnumerable<User>>(userContent) ?? Array.Empty<User>()).ToList();
+        Debug.Assert(context.Users != null, "context.Users != null");
+        await context.Users.AddRangeAsync(users);
+        await context.SaveChangesAsync();
+    }
 }
