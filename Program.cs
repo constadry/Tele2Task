bae833695d1df187964c1c03f6420f5b23639cca
constadry <behappydtworry@gmail.com>
2022-03-06 11:36:35 +0300

    feat: added DI

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Tele2Task;
 using Tele2Task.Contexts;
+using Tele2Task.Repositories;
+using Tele2Task.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,13 @@ var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 var version = ServerVersion.AutoDetect(connection);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connection, version));
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 var scope = app.Services.CreateScope();
@@ -30,6 +38,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "This is test task api app for tele2 company, wow!");
 
 app.Run();
